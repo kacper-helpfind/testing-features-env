4b0e08de5bdee5449fb31e0db2d7ec5a0509f670
Kacper Adler <kacper.adler@helpfind.pl>
2024-06-28 15:35:23 +0200

    fix: Api paths

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,11 +2,11 @@ import axios, { AxiosInstance } from "axios";
 import { Configuration, PetApi } from "./generated";
 
 const config = new Configuration({
-  basePath: "https://petstore3.swagger.io/api/v3/openapi.json",
+  basePath: "https://petstore3.swagger.io",
 });
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: "https://petstore3.swagger.io/api/v3",
 });
 
-export const petApiClient = new PetApi(config, "", axiosInstance);
+export const petApiClient = new PetApi(config, "/pet", axiosInstance);
